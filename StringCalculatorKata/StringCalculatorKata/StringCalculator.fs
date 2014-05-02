8ba2f188fac177415e06f42bf9853adbb900b910
Andrew Russell <andrew.russell@chillisoft.co.za>
2014-05-02 08:19:59 +0200

    first commit of stringcalculatorkata in f#

---
+++ StringCalculatorKata/StringCalculatorKata/StringCalculator.fs
@@ -0,0 +1,60 @@
+﻿namespace StringCalculatorKata
+open System
+open NUnit.Framework
+open FsUnit
+
+type StringCalculator() =
+    let split (delimiters: string[]) (input: string) =
+        input.Split(delimiters, StringSplitOptions.None)
+
+    let getDelimiterSection (input:string) =
+        if input.StartsWith("//") then
+            input.Substring(0, input.IndexOf("\n") + 1)
+        else
+            ""
+    let getNumberSection (input:string) (delimiterSection: string) =
+        if delimiterSection = "" then input
+        else
+            input.Replace(delimiterSection, "")
+
+    let getCustomDelimiters (delimiterSection:string) =
+        delimiterSection.Replace("//", "").Replace("\n","").TrimStart('[').TrimEnd(']') 
+            |> split [|"]["|] 
+            |> Array.toList
+
+    let getDelimiters (delimiterSection: string) =
+        let defaultDelimiters = [",";"\n"]
+        if delimiterSection = "" then Seq.toArray defaultDelimiters
+        else
+            let customDelimiters = delimiterSection |> getCustomDelimiters
+            let allDelimiters = customDelimiters @ defaultDelimiters
+            Seq.toArray customDelimiters
+ 
+    let concatNegatives negatives =
+        (negatives |> Seq.map (fun n -> n.ToString()) |> String.concat ",")
+
+    let checkForNegatives numbers =
+        let negatives = Seq.where (fun n -> n<0) numbers
+        if (Seq.length negatives = 0) then numbers
+        else
+            let errorMessage = "negatives not allowed:" +  (concatNegatives negatives)
+            failwith errorMessage
+    let filterLargeNumbers numbers =
+        numbers |> Seq.filter (fun n -> n<=1000)
+                              
+    let doAdd (input: string) =
+        let delimiterSection = getDelimiterSection input
+        let numberSection = getNumberSection input delimiterSection
+        let delimiters = getDelimiters delimiterSection
+        
+        split delimiters numberSection
+        |> Seq.map Int32.Parse
+        |> checkForNegatives
+        |> filterLargeNumbers
+        |> Seq.sum
+
+    member this.Add(input:string) =
+        match input with 
+            | "" -> 0
+            | _ -> doAdd input
+
---
+++ StringCalculatorKata/StringCalculatorKata/TestStringCalculator.fs
@@ -0,0 +1,85 @@
+﻿namespace TestStringCalculatorKata
+open System
+
+open NUnit.Framework
+open FsUnit
+open StringCalculatorKata
+
+[<TestFixture>]
+type StringCalculatorTests() =
+    let Create() = new StringCalculator()
+
+    [<Test>]
+    member test.``Add, given empty string, should return 0``() =
+        let sc = Create()
+        let input = ""
+        sc.Add(input) |> should equal 0
+
+    [<Test>]
+    member test.``Add, given single char number string, should return number``() =
+        let sc = Create()
+        let input = "1"
+        sc.Add(input) |> should equal 1
+
+    [<Test>]
+    member test.``Add, given multi char number string, should return number``() =
+        let sc = Create()
+        let input = "123"
+        sc.Add(input) |> should equal 123
+
+    [<Test>]
+    member test.``Add, given two comma delimited numbers in string, should return sum``() =
+        let sc = Create()
+        let input = "1,2"
+        sc.Add(input) |> should equal 3
+
+    [<Test>]
+    member test.``Add, given many comma delimited numbers in string, should return sum``() =
+        let sc = Create()
+        let input = "1,2,25"
+        sc.Add(input) |> should equal 28
+
+    [<Test>]
+    member test.``Add, given numbers string with comma and newline delimiters, should return sum``() =
+        let sc = Create()
+        let input = "3,5\n7"
+        sc.Add(input) |> should equal 15
+
+    [<Test>]
+    member test.``Add, given numbers string with single customer delimiter, should return sum``() =
+        let sc = Create()
+        let input = "//;\n2;4;6"
+        sc.Add(input) |> should equal 12
+
+    [<Test>]
+    member test.``Add, given delimited number string with negative number, should throw``() =
+        let sc = Create()
+        let input = "1,-2,3"
+        let ex = Assert.Throws<Exception>(fun () -> sc.Add(input)|> ignore) 
+        ex.Message |> should equal "negatives not allowed:-2"
+
+    [<Test>]
+    member test.``Add, given delimited number string with negative numbers, should throw``() =
+        let sc = Create()
+        let input = "1,-2,-3"
+        let ex = Assert.Throws<Exception>(fun () -> sc.Add(input)|> ignore) 
+        ex.Message |> should equal "negatives not allowed:-2,-3"        
+
+    [<Test>]
+    member test.``Add, given delimited number string with large numbers, should filter large numbers and sum``() =
+        let sc = Create()
+        let input = "1,2,1000,1001,1002"
+        sc.Add(input) |> should equal 1003
+
+    [<Test>]
+    member test.``Add, given number string with multi char delimiter, should sum``() =
+        let sc = Create()
+        let input = "//[***]\n5***5***5"
+        sc.Add(input) |> should equal 15
+
+    [<Test>]
+    member test.``Add, given number string with multiple delimiters, should sum``() =
+        let sc = Create()
+        let input = "//[*][$][?]\n4*4$4?4"
+        sc.Add(input) |> should equal 16
+
